c8f24fa479bd0a9e82a73ff2ffb26f97e62e9530
FeliciaMarlove <missflorencemary@hotmail.com>
2021-01-10 09:25:38 +0100

    Added implements Serializable, constructors, toString, hashCode and equals to Model classes

--- src/main/java/com/soprasteria/fleet/models/CarEmployeeLinking.java
+++ src/main/java/com/soprasteria/fleet/models/CarEmployeeLinking.java
@@ -3,11 +3,13 @@ package com.soprasteria.fleet.models;
 import com.soprasteria.fleet.models.primaryKeys.CarEmployeeCompositePK;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "car_employee_linking")
-public class CarEmployeeLinking {
+public class CarEmployeeLinking implements Serializable {
     @EmbeddedId
     private CarEmployeeCompositePK carEmployeeCompositePK;
 
@@ -87,5 +89,41 @@ public class CarEmployeeLinking {
 
     public void setStaffMember(StaffMember staffMember) {
         this.staffMember = staffMember;
+    }
+
+    public CarEmployeeLinking(LocalDate startDate, LocalDate endDate, boolean isOngoing, Car car, StaffMember staffMember) {
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.isOngoing = isOngoing;
+        this.car = car;
+        this.staffMember = staffMember;
+        this.carEmployeeCompositePK = new CarEmployeeCompositePK(car.getPlateNumber(), staffMember.getStaffMemberId());
+    }
+
+    public CarEmployeeLinking() {
+    }
+
+    @Override
+    public String toString() {
+        return "CarEmployeeLinking{" +
+                "carEmployeeCompositePK=" + carEmployeeCompositePK +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", kmAtEnd=" + kmAtEnd +
+                ", isOngoing=" + isOngoing +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarEmployeeLinking that = (CarEmployeeLinking) o;
+        return carEmployeeCompositePK.equals(that.carEmployeeCompositePK);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carEmployeeCompositePK);
     }
 }
--- src/main/java/com/soprasteria/fleet/models/primaryKeys/CarEmployeeCompositePK.java
+++ src/main/java/com/soprasteria/fleet/models/primaryKeys/CarEmployeeCompositePK.java
@@ -3,6 +3,7 @@ package com.soprasteria.fleet.models.primaryKeys;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class CarEmployeeCompositePK implements Serializable {
@@ -11,4 +12,34 @@ public class CarEmployeeCompositePK implements Serializable {
 
     @Column(name = "staff_member_id")
     private Integer staffMemberId;
+
+    public CarEmployeeCompositePK(String carPlate, Integer staffMemberId) {
+        this.carPlate = carPlate;
+        this.staffMemberId = staffMemberId;
+    }
+
+    public CarEmployeeCompositePK() {
+    }
+
+    @Override
+    public String toString() {
+        return "CarEmployeeCompositePK{" +
+                "carPlate='" + carPlate + '\'' +
+                ", staffMemberId=" + staffMemberId +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CarEmployeeCompositePK that = (CarEmployeeCompositePK) o;
+        return carPlate.equals(that.carPlate) &&
+                staffMemberId.equals(that.staffMemberId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(carPlate, staffMemberId);
+    }
 }
