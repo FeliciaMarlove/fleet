e419f63771b8d0ad922780e40596b8d633ccb1b1
FeliciaMarlove <missflorencemary@hotmail.com>
2021-01-13 17:09:23 +0100

    CarController

---
+++ src/main/java/com/soprasteria/fleet/controllers/CarController.java
@@ -0,0 +1,53 @@
+package com.soprasteria.fleet.controllers;
+
+import com.soprasteria.fleet.dto.CarDTO;
+import com.soprasteria.fleet.services.interfaces.CarService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/car")
+@CrossOrigin
+public class CarController {
+    private final CarService service;
+
+    public CarController(CarService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{id}")
+    public CarDTO getCar(@PathVariable("id") String id) {
+        return service.read(id);
+    }
+
+    @GetMapping
+    public List<CarDTO> getAllCars() {
+        return service.readAll();
+    }
+
+    @GetMapping("/active")
+    public List<CarDTO> getAllCarsActive() {
+        return service.readAllActive();
+    }
+
+    @GetMapping("/archived")
+    public List<CarDTO> getAllCarsArchived() {
+        return service.readAllArchived();
+    }
+
+    @PostMapping
+    public CarDTO createCar(@RequestBody CarDTO carDTO) {
+        return service.create(carDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public String archiveCar(@PathVariable("id") String id) {
+        return service.delete(id);
+    }
+
+    @PutMapping
+    public CarDTO updateCar(@RequestBody CarDTO carDTO) {
+        return service.update(carDTO);
+    }
+}
