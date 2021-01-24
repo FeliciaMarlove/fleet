a1778637acc05eb76f36c6c196ee934218e6942d
FeliciaMarlove <missflorencemary@hotmail.com>
2021-01-24 11:22:00 +0100

    Changed retrieval of Tank fillings with discrepancies to Named query to avoid issues with null values

--- src/main/java/com/soprasteria/fleet/repositories/TankFillingRepository.java
+++ src/main/java/com/soprasteria/fleet/repositories/TankFillingRepository.java
@@ -1,7 +1,15 @@
 package com.soprasteria.fleet.repositories;
 
+import com.soprasteria.fleet.models.Car;
 import com.soprasteria.fleet.models.TankFilling;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TankFillingRepository extends CrudRepository<TankFilling, Integer> {
+    @Query(
+            value = "SELECT * FROM tank_filling t WHERE t.is_discrepancy = true",
+            nativeQuery = true)
+    List<TankFilling> selectTankFillingWhereDiscrepancyIsTrue();
 }
