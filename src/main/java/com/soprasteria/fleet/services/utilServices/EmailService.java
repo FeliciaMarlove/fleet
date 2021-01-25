2af93d929f7907aa7172334f0b41f5da25a1b36a
FeliciaMarlove <missflorencemary@hotmail.com>
2021-01-25 11:51:28 +0100

    created a method to send emails with attachments, not tested

--- src/main/java/com/soprasteria/fleet/services/utilServices/EmailService.java
+++ src/main/java/com/soprasteria/fleet/services/utilServices/EmailService.java
@@ -1,37 +1,37 @@
 package com.soprasteria.fleet.services.utilServices;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.util.List;
 import java.util.Properties;
 
 @Component
 public class EmailService {
     @Autowired
     private JavaMailSender emailSender;
+    private static final String username = "fleet.tfe.2021@gmail.com";
+    private static final String password = "yabctpqummdkmeyc";
+    private static final Properties props;
 
-    public void sendSimpleMessage(String to, String subject, String text) {
-        final String username = "fleet.tfe.2021@gmail.com";
-        final String password = "yabctpqummdkmeyc";
-
-        Properties props = new Properties();
+    static {
+        props = new Properties();
         props.put("mail.smtp.auth", true);
         props.put("mail.smtp.starttls.enable", true);
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
         props.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+    }
 
-        Session session = Session.getInstance(props,
-                new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-
+    public void sendSimpleMessage(String to, String subject, String text) {
+        Session session = setSession(to, subject, text);
         try {
 
             Message message = new MimeMessage(session);
@@ -43,10 +43,49 @@ public class EmailService {
 
             Transport.send(message);
 
-            System.out.println("Done");
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+    TODO ? List of attachments might be of type byte[] according to storage chosen for files
+     */
+    public void sendMessageWithAttachments(String to, String subject, String text, List<String> attachments) {
+        Session session = setSession(to, subject, text);setSession(to, subject, text);
+        try {
+
+            MimeMessage message = new MimeMessage(session);
+
+            MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+            helper.setFrom(username);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+
+            for (String pathToAttachment: attachments) {
+                FileSystemResource file
+                        = new FileSystemResource(new File(pathToAttachment));
+                helper.addAttachment(file.getFilename(), file);
+            }
+
+            // emailSender.send(message); // if next line not working try this
+
+            Transport.send(message);
 
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private Session setSession(String to, String subject, String text) {
+        Session session = Session.getInstance(props,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+        return session;
     }
 }
