0b69f0f188f2d3d6f44842d28bb75df894ae5f95
FeliciaMarlove <missflorencemary@hotmail.com>
2021-02-21 15:45:52 +0100

    Fix(StaffMemberService) retrieving current car of the staff member - repository query methods have to return type of the repository, moved repository methods from StaffMemberRepository to CarRepository

--- src/main/java/com/soprasteria/fleet/repositories/StaffMemberRepository.java
+++ src/main/java/com/soprasteria/fleet/repositories/StaffMemberRepository.java
@@ -9,13 +9,5 @@ import java.util.List;
 import java.util.Optional;
 
 public interface StaffMemberRepository extends CrudRepository<StaffMember, Integer> {
-    @Query(
-            value = "SELECT * FROM car c WHERE c.staff_id = ?1",
-            nativeQuery = true)
-    List<Car> selectCarWhereStaffIdIs(Integer staffId);
 
-    @Query(
-            value = "SELECT * FROM car c WHERE c.staff_id = ?1 AND c.ongoing = true",
-            nativeQuery = true)
-    Optional<Car> selectCarWhereStaffIdIsAndOngoingTrue(Integer staffId);
 }
