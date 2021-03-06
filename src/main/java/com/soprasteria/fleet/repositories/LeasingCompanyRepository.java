4abb5f5856e0b6be69c68497e90e7a5a416d4491
FeliciaMarlove <missflorencemary@hotmail.com>
2021-03-06 15:51:37 +0100

    Improvement(LeasingCompany) Change methods to query methods

--- src/main/java/com/soprasteria/fleet/repositories/LeasingCompanyRepository.java
+++ src/main/java/com/soprasteria/fleet/repositories/LeasingCompanyRepository.java
@@ -1,7 +1,15 @@
 package com.soprasteria.fleet.repositories;
 
 import com.soprasteria.fleet.models.LeasingCompany;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface LeasingCompanyRepository extends CrudRepository<LeasingCompany, Integer> {
+    @Query(
+            value = "SELECT * FROM leasing_company l WHERE l.is_active = true",
+            nativeQuery = true
+    )
+    List<LeasingCompany> selectFromLeasingCompanyWhereActiveTrue();
 }
