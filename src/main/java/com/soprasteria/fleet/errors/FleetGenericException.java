422b8e814a880a565d0e0664536bcbcd90d4612e
Florence <missflorencemary@hotmail.com>
2021-07-04 21:12:36 +0200

    Error handling(services, exception controller) change services to write logs instead of throw

--- src/main/java/com/soprasteria/fleet/errors/FleetGenericException.java
+++ src/main/java/com/soprasteria/fleet/errors/FleetGenericException.java
@@ -1,18 +1,13 @@
 package com.soprasteria.fleet.errors;
 
-import com.soprasteria.fleet.services.utilServices.AzureBlobLoggingService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public class FleetGenericException extends RuntimeException {
-    @Autowired
-    private AzureBlobLoggingService azureLogService;
 
     public FleetGenericException() {
-        super();
+
     }
 
     public FleetGenericException(String message) {
-        azureLogService.writeToLoggingFile(message);
     }
 
 }
--- src/main/java/com/soprasteria/fleet/errors/FleetItemNotFoundException.java
+++ src/main/java/com/soprasteria/fleet/errors/FleetItemNotFoundException.java
@@ -1,17 +1,11 @@
 package com.soprasteria.fleet.errors;
 
-import com.soprasteria.fleet.services.utilServices.AzureBlobLoggingService;
-import org.springframework.beans.factory.annotation.Autowired;
-
 public class FleetItemNotFoundException extends FleetGenericException {
-    @Autowired
-    private AzureBlobLoggingService azureLogService;
 
     public FleetItemNotFoundException() {
-        super();
+
     }
 
     public FleetItemNotFoundException(String message) {
-        azureLogService.writeToLoggingFile(message);
     }
 }
