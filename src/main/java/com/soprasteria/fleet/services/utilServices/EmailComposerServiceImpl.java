4137bdf536c41c2e29814d4073e884d285b4e11d
Florence <missflorencemary@hotmail.com>
2021-10-10 13:10:44 +0200

    add SAS token for reading permission in inspection email

--- src/main/java/com/soprasteria/fleet/services/utilServices/EmailComposerServiceImpl.java
+++ src/main/java/com/soprasteria/fleet/services/utilServices/EmailComposerServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Component;
 @Component
 public final class EmailComposerServiceImpl implements EmailComposerService {
     private final AzureBlobLoggingService azureBlobLoggingService;
+    private final static String INSPECTION_BLOB_READ_ACCESS_TOKEN = "?sp=r&st=2021-10-10T11:06:15Z&se=2021-11-30T20:06:15Z&sv=2020-08-04&sr=c&sig=RreBS4JHzW0u4R3fIpVZIPDlEmfIh49uk3Z5dyCUido%3D";
 
     public EmailComposerServiceImpl(AzureBlobLoggingService azureBlobLoggingService) {
         this.azureBlobLoggingService = azureBlobLoggingService;
@@ -20,12 +21,11 @@ public final class EmailComposerServiceImpl implements EmailComposerService {
     @Override
     public String writeEmailToFleetManagerAboutDiscrepancy(TankFilling tankFilling) {
         StringBuilder stringBuilder = new StringBuilder();
-        boolean staffMember = tankFilling.getCar().getStaffMember() != null;
         stringBuilder.append("A discrepancy has been detected:")
                 .append("\nDate: ").append(tankFilling.getDateTimeFilling())
                 .append("\nCar: ").append(tankFilling.getCar().getPlateNumber())
-                .append("\nStaff member: ").append(staffMember ? tankFilling.getCar().getStaffMember().getStaffFirstName() : "{a problem occurred while retrieving the staff member")
-                .append(" ").append(staffMember ? tankFilling.getCar().getStaffMember().getStaffLastName().toUpperCase() : "")
+                .append("\nStaff member: ").append(tankFilling.getCar().getStaffMember().getStaffFirstName())
+                .append(" ").append(tankFilling.getCar().getStaffMember().getStaffLastName().toUpperCase())
                 .append("\nDiscrepancy: ").append(tankFilling.getDiscrepancyType());
 
         switch (tankFilling.getDiscrepancyType()) {
@@ -76,7 +76,7 @@ public final class EmailComposerServiceImpl implements EmailComposerService {
             stringBuilder.append(" revealed no damage.");
         } else {
             stringBuilder.append(" revealed some damage.")
-                    .append("\nPlease consult the report at " + inspection.getInspectionReportFile());
+                    .append("\nPlease consult the report at " + inspection.getInspectionReportFile() + INSPECTION_BLOB_READ_ACCESS_TOKEN);
             stringBuilder.append("\nYou will receive information about the financial implication in the coming days.");
         }
         stringBuilder.append("\nBest regards,\nThe fleet manager");
