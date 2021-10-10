9d4b9ba8d5ad8a30cb7566cb396db50bb6154218
Florence <missflorencemary@hotmail.com>
2021-10-10 14:51:04 +0200

    clean + comment

--- src/main/java/com/soprasteria/fleet/errors/FleetItemNotFoundException.java
+++ src/main/java/com/soprasteria/fleet/errors/FleetItemNotFoundException.java
@@ -1,5 +1,8 @@
 package com.soprasteria.fleet.errors;
 
+/**
+ * Represent an exception caused by an item not found
+ */
 public final class FleetItemNotFoundException extends FleetGenericException {
 
     public FleetItemNotFoundException() {
